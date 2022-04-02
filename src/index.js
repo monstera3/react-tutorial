be11a50647b0aa6c6fe79b7a3cd232e7bb89170f
natsumi <aka.723n@gmail.com>
2022-04-02 18:11:31 +0900

    ボタンをクリックしたらclickと表示される

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={function () {
+        console.log('click');
+      }} >
         {this.props.val}
       </button>
     );
